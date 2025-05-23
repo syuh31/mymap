8e24c9048b3b7534d05b8954dafbf2781d3773bc
syuh31 <iceant6b@gmail.com>
2025-05-23 16:41:17 +0900

    update

--- src/data/themes.ts
+++ src/data/themes.ts
@@ -2,6 +2,24 @@ import { MapTheme } from '../types';
 
 export const mapThemes: MapTheme[] = [
   {
+    id: 'hakataeki',
+    name: '博多駅',
+    description: '博多駅',
+    center: [33.58981221397687, 130.42088233754143],
+    zoom: 16,
+    dataFile: '/data/hakataeki.csv',
+    image: 'https://images.pexels.com/photos/2676331/pexels-photo-2676331.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    id: 'kurume',
+    name: '久留米',
+    description: '久留米',
+    center: [33.3137512,130.5236905],
+    zoom: 14,
+    dataFile: '/data/kurume.csv',
+    image: 'https://images.pexels.com/photos/2676331/pexels-photo-2676331.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
     id: 'hita',
     name: '日田',
     description: 'Explore the historical city of Hita with its traditional buildings and scenic spots',
